dc57c84c2c04b7c29bbb1ad2ff68e7d088f96a9e
loegnah <loegnah@gmail.com>
2022-04-29 01:31:45 +0900

    [css] Select Item

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">Nice</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Super Table</span>
+          <span className="font-semibold">$15000</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span className="text-gray-500">Great Pants</span>
+          <span className="font-semibold">$999</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$15999</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-5 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
